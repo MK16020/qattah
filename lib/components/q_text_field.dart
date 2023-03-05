b45e1b06bfaaa4816adfb0fd29ca42503d8983cf
MK16020 <61877101+MK16020@users.noreply.github.com>
2023-03-05 10:39:53 +0300

    add pages

---
+++ lib/components/q_text_field.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QTextField extends StatelessWidget {
+  const QTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
