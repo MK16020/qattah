4308861682fd662c8138cb14f4088d63a0817f07
sara <humayed99@gmail.com>
2023-03-05 14:52:10 +0300

    Added constants file

---
+++ lib/constants/qtext.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const QHeader1 = TextStyle(fontSize: 40);
+const QButtonSize = TextStyle(fontSize: 20);
+const QAppBarSize = TextStyle(fontSize: 32);
+const QSheader2 = TextStyle(fontSize: 24);
+const QRegularTextSize = TextStyle(fontSize: 16);
+const QSmallerTextSize = TextStyle(fontSize: 12);
