ff2214d7256b98de6664b5dba5ac3d80abdbd8b2
MK16020 <61877101+MK16020@users.noreply.github.com>
2023-03-05 15:57:51 +0300

    account option page

---
+++ lib/components/q_account_option.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QAccountOption extends StatelessWidget {
+  const QAccountOption({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
